4adcef6e8ae2653b5c7d2560abc979bcdc3f5851
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-05-20 15:14:49 -0400

    commit with most code working. light-dark function, final styling, and responsive design remain

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,9 +1,7 @@
 function retrievePosts(){
-    // const blogEntry = document.querySelector(".post");
-    const postList = document.querySelector("#postList");
     const posts = JSON.parse(localStorage.getItem("blogPosts"));
+    postList = document.querySelector("#postList"); //removing const forces this to be a global variable
     
-    // postList.innerHTML = "";
     for (let i = 0; i < posts.length; i++) {
         const post = posts[i];
         const postAuthor = post.postAuthor;
@@ -11,21 +9,35 @@ function retrievePosts(){
         const postContent = post.postContent;
 
         const article = document.createElement("article");
-        article.innerHTML="<article>\n<div class=\"titleauthor\">\n<p>" + postTitle + "</p>\n<p>" + postAuthor + "</p>\n</div>\n<div class=\"content\">\n<p>" + postContent + "</p>\n</div>\n</article>";
+        article.innerHTML="\n<div class=\"titleauthor\">\n<p>" + postTitle + "</p>\n<p>" + postAuthor + "</p>\n</div>\n<div class=\"content\">\n<p>" + postContent + "</p>\n</div>\n";
         article.setAttribute("data-index", i);
-        
         postList.appendChild(article);
     };
-};
+    
+    const items = document.querySelectorAll(".content");
+    items.forEach(item => {
+        item.addEventListener("click", () => {
+            item.classList.toggle("clicked");
+        })
+    });
 
+    const clickedButton = document.querySelector("#newPost")
+    clickedButton.addEventListener("click", () => {
+        window.open("index.html");
+    }
+    )
+};
 
     //✔️retrieve data from localStorage
     //✔️retrieve object from data
     //✔️asssign each key value from object to a variable
-    // STILL HAVING TROUBLE FIRING retrievePosts() ON PAGE LOAD.
     //✔️insert variable for author into div
     //✔️insert variable for title into same div
-    // insert variable for content into new div
-    // wrap pair of divs into article
-    // display article
-    // repeat for each item in data
+    //✔️insert variable for content into new div
+    //✔️wrap pair of divs into article
+    //✔️display article
+    //✔️repeat for each item in data
+    //✔️display selected post
+    //✔️return to input form to add another post
+    // activate dark-mode
+    // style
